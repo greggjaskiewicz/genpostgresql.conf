93321cb5083316c89f53b0c05ca43ed8aa35947a
Greg Jaskiewicz <gryzman@gmail.com>
2013-03-23 00:11:00 +0000

    forgot about this cruicial piece of script - it runs the actual test

--- run_a_test.sh
+++ run_a_test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 time=15
-connections=20
+connections=40
 
 select=`/usr/local/pgsql/bin/pgbench -c $connections    -T $time  gj | grep tps | grep including | awk '{x=$3+0.5; y=int(x); print y; }'`
 insert=`/usr/local/pgsql/bin/pgbench -c $connections -S -T $time  gj | grep tps | grep including | awk '{x=$3+0.5; y=int(x); print y; }'`
 
-f=$(($select+$insert))
+f=$(($(($select))+$(($insert))))
 
-echo $f
+echo $(($f))
 
