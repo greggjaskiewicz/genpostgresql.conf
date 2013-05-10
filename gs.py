999f07587d4f048eb37fd467333efe003a5b0278
Greg Jaskiewicz <gryzman@gmail.com>
2013-05-10 23:02:13 +0100

    Refactor python files slightly to use 4 spaces instead of 2     And, few tweaks here and there to satisfy our friend pylint

--- gs.py
+++ gs.py
@@ -7,7 +7,7 @@ Includes only performance related options - to keep it simple
 Licenced under PostgreSQL BSD Licence
 
 TODO:
-* split gentic stuff into separate agnostic class
+* split generic stuff into separate agnostic class
 * store results and so on in database maybe (sqllite would do)
 * save/restore of state ?
 * separate configuration file
@@ -16,143 +16,142 @@ TODO:
 """
 
 from postgres_config import PostgresConfig
-import random
 import copy
-import shlex, subprocess
+import subprocess
 import os
 
-
-population_size=24
+# Bad, bad globals
+population_size = 24
 current_population = []
-new_pg_config="/tmp/new_pg_config.conf"
+new_pg_config = "/tmp/new_pg_config.conf"
+
 
 def generate_initial_population(the_population_size):
-  newpopulation = []
-  while the_population_size > 0:
-    the_population_size = the_population_size-1
-    p = PostgresConfig()
-    # maybe use some less random methods to generate different postgrsql configurations
-    p.set_random()
-    newpopulation.append(p)
+    newpopulation = []
+    while the_population_size > 0:
+        the_population_size = the_population_size - 1
+        p = PostgresConfig()
+        # maybe use some less random methods to generate different PostgreSQL configurations
+        p.set_random()
+        newpopulation.append(p)
 
-  return newpopulation
+    return newpopulation
 
 
 def run_test(g):
-  #g.result = 10.0
-  #return 10.0
+    # g.result = 10.0
+    # return 10.0
 
-  g.print_out()
-  """run the test 3 times, avg out the results """
-  results=[]
-  for x in range(3):
-    output1 = subprocess.Popen(["./run_a_test.sh"], stdout=subprocess.PIPE).communicate()[0]
-    if (len(output1)):
-      results.append(int(output1))
+    g.print_out()
+    """run the test 3 times, avg out the results """
+    results = []
+    for _ in range(3):
+        output1 = subprocess.Popen(["./run_a_test.sh"], stdout=subprocess.PIPE).communicate()[0]
+        if (len(output1)):
+            results.append(int(output1))
 
-  if (len(results)):
-    g.result = sum(results) / float(len(results))
-  else:
-    g.result = 0
+    if (len(results)):
+        g.result = sum(results) / float(len(results))
+    else:
+        g.result = 0
 
-  print g.result
+    print g.result
 
 
 def test_population(the_test_population):
-  for guy in the_test_population:
-    reload_postgresql_for(guy)
-    run_test(guy)
+    for guy in the_test_population:
+        reload_postgresql_for(guy)
+        run_test(guy)
 
 
 def reload_postgresql_for(guy):
-  try:
-    os.remove(new_pg_config)
-  except:
-    """foo"""
-  finally:
-    """dont care if didn't exist"""
-  guy.print_out_to_file(new_pg_config)
+    try:
+        os.remove(new_pg_config)
+    except:
+        """foo"""
+    finally:
+        """dont care if didn't exist"""
+    guy.print_out_to_file(new_pg_config)
 
-  subprocess.call(["./reload_postgresql_with_new_config.sh", new_pg_config])
-
+    subprocess.call(["./reload_postgresql_with_new_config.sh", new_pg_config])
 
 
 def eliminate_weakests(the_test_population):
-  """ order by score, remove half """
-  #remove ones with result equal 0
-  for guy in the_test_population:
-    if guy.result < 1:
-      the_test_population.remove(guy)
+    """ order by score, remove half """
+    # remove ones with result equal 0
+    for guy in the_test_population:
+        if guy.result < 1:
+            the_test_population.remove(guy)
 
-  the_test_population = sorted(the_test_population, key=lambda guy: guy.result)
-  the_test_population.reverse()
-  for guy in the_test_population:
-    print guy.result
-  print '\n'
-  """
-     maybe use average as cut off point ?
-     but then, what about new generation when there's not enough parents ?
-     We could also use historical median/average as a cut off point. But this might be bit
-     problematic
-  """
-  the_test_population = the_test_population[0:population_size/2]
+    the_test_population = sorted(the_test_population, key=lambda guy: guy.result)
+    the_test_population.reverse()
+    for guy in the_test_population:
+        print guy.result
+    print '\n'
+    """
+       maybe use average as cut off point ?
+       but then, what about new generation when there's not enough parents ?
+       We could also use historical median/average as a cut off point. But this might be bit
+       problematic
+    """
+    the_test_population = the_test_population[0:population_size / 2]
 
-  return the_test_population
-
+    return the_test_population
 
 
 def mutate_new_population(parent_population):
-  """ take what's there, and mate them until you get at most population_size """
-  new_population = []
-  for x in range(len(parent_population)):
-    for y in range(x):
-      if (x == y):
-        continue #need two different mates!
-      newguy = copy.deepcopy(parent_population[x])
-      newguy.cross_over(parent_population[y])
-      newguy.mutate_random(5, 3)
-      newguy.result = 0
-      new_population.append(newguy)
-      if len(new_population) == population_size:
-        return new_population
-  return new_population
+    """
+    take what's there, and mate them until you get at most population_size
+    """
+    new_population = []
+    for x in range(len(parent_population)):
+        for y in range(x):
+            if (x == y):
+                continue    # need two different mates!
+            newguy = copy.deepcopy(parent_population[x])
+            newguy.cross_over(parent_population[y])
+            newguy.mutate_random(5, 3)
+            newguy.result = 0
+            new_population.append(newguy)
+            if len(new_population) == population_size:
+                return new_population
+    return new_population
 
 
 def dump_population(x, population):
-  file_name = 'population_'+str(x)+'.ppldump'
-  f = open(file_name, 'w')
+    file_name = 'population_' + str(x) + '.ppldump'
+    f = open(file_name, 'w')
 
-  for guy in population:
-    config = guy.get_config()
-    print >> f, config
-    print >> f, 'score = '+str(guy.result)
-    print >> f, '\n'
+    for guy in population:
+        config = guy.get_config()
+        print >> f, config
+        print >> f, 'score = ' + str(guy.result)
+        print >> f, '\n'
 
-  f.close()
+    f.close()
 
 
 def main():
-  current_population = generate_initial_population(population_size)
+    current_population = generate_initial_population(population_size)
 
-  print 'foo1'
+    print 'foo1'
 
-  for i in range(1000):
-    print '\n===============\nNew Population %d, %d members\n===============\n' % (i, len(current_population))
+    for i in range(1000):
+        print '\n===============\nNew Population %d, %d members\n===============\n' % (i, len(current_population))
 
-    """dump this population, before it is run, it will eventually get overriden with the one with results"""
-    dump_population(i, current_population)
+        """
+        dump this population, before it is run, it will eventually get overriden with the one with results
+        """
+        dump_population(i, current_population)
 
-    test_population(current_population)
-    dump_population(i, current_population)
+        test_population(current_population)
+        dump_population(i, current_population)
 
-    living_population = eliminate_weakests(current_population)
-    current_population = mutate_new_population(living_population)
+        living_population = eliminate_weakests(current_population)
+        current_population = mutate_new_population(living_population)
 
-  for guy in current_population:
-   guy.print_out()
+    for guy in current_population:
+        guy.print_out()
 
 if __name__ == '__main__':
-  main()
-
-
-
+    main()
--- postgres_config.py
+++ postgres_config.py
@@ -4,151 +4,149 @@ Includes only performance related options - to keep it simple
 
 (c) Greg Jaskiewicz 2013
 
-Licenced under PostgreSQL BSD Licence
+Licensed under PostgreSQL BSD Licence
 
 
 TODO list:
 * generate baseline postgresql.confs more sensibly then just random.
 * improve the way random mutations are done
-* more postgresql options
+* more PostgreSQL options
 * serialise to database, deserialise from database
 
 """
 
 import random
-import pickle
-
-class PostgresConfig:
-  """options"""
-
-  _options = {'shared_buffers':'memory', 'effective_cache_size':'memory', 'checkpoint_segments':'uint',
-              'checkpoint_completion_target':'0.0to1.0', 'default_statistics_target':'uint',
-              'maintenance_work_mem':'memory', 'work_mem':'memory', 'wal_buffers':'memory',
-              'random_page_cost':'0.0to50.0', 'seq_page_cost':'0.0to50.0', 'cpu_tuple_cost':'0.0to50.0',
-              'cpu_index_tuple_cost':'0.0to50.0', 'cpu_operator_cost':'0.0to50.0'
-  }
 
 
-  def __init__(self):
-    """ constructor """
-    self.config = self._options.copy()
-    self.result = 0.0
-    for option in self.config:
-      self.config[option] = 0
+class PostgresConfig(object):
+    """options"""
 
+    _options = {
+                'work_mem': 'memory',
+                'wal_buffers': 'memory',
+                'seq_page_cost': '0.0to50.0',
+                'cpu_tuple_cost': '0.0to50.0',
+                'shared_buffers': 'memory',
+                'random_page_cost': '0.0to50.0',
+                'cpu_operator_cost': '0.0to50.0',
+                'checkpoint_segments': 'uint',
+                'maintenance_work_mem': 'memory',
+                'effective_cache_size': 'memory',
+                'cpu_index_tuple_cost': '0.0to50.0',
+                'default_statistics_target': 'uint',
+                'checkpoint_completion_target': '0.0to1.0'
+    }
 
-  def _random_for_type(self, thetype):
-    if thetype == 'memory':
-      """ integer between 1kb and 1024MB """
-      val = random.randint(1, 1024*1024*1024)
-      return val
-    elif thetype == '0.0to1.0':
-      return round(random.random(), 3)
-    elif thetype == '0.0to50.0':
-      return round(random.uniform(0.01, 50.0), 3)
-    elif thetype == 'uint':
-      return random.randint(1, 1024)
-    else:
-      return '0'
+    def __init__(self):
+        """ constructor """
+        self.config = self._options.copy()
+        self.result = 0.0
+        for option in self.config:
+            self.config[option] = 0
 
+    def _random_for_type(self, thetype):
+        if thetype == 'memory':
+            """ integer between 1kb and 1024MB """
+            val = random.randint(1, 1024 * 1024 * 1024)
+            return val
+        elif thetype == '0.0to1.0':
+            return round(random.random(), 3)
+        elif thetype == '0.0to50.0':
+            return round(random.uniform(0.01, 50.0), 3)
+        elif thetype == 'uint':
+            return random.randint(1, 1024)
+        else:
+            return '0'
 
-  def set_random(self):
-    self.config = self._options.copy()
-    for option in self.config:
-      value = self._random_for_type(self.config[option])
-      self.config[option] = value
+    def set_random(self):
+        self.config = self._options.copy()
+        for option in self.config:
+            value = self._random_for_type(self.config[option])
+            self.config[option] = value
 
+    def cross_over(self, mate):
+        """
+        pick randomly half of params from the 'mate'
+        """
+        newconfig_keys = self.config.keys()
+        random.shuffle(newconfig_keys)
 
-  """ pick randomly half of params from the 'mate' """
-  def cross_over(self, mate):
-    newconfig_keys = self.config.keys()
-    random.shuffle(newconfig_keys)
+        """
+        now that we have randomised newconfig dictionary go through it
+        and every second option shall be replaced with mate values
+        """
+        i = 0
+        newconfig = dict()
+        for k in newconfig_keys:
+            if (i % 2):
+                newconfig[k] = mate.config[k]
+            else:
+                newconfig[k] = self.config[k]
+            i = i + 1
 
-    """
-        now that we have randomised newconfig dictionary
-        go through it, and every second option shall be replaced with mate values
-    """
-    i = 0
-    newconfig = dict()
-    for k in newconfig_keys:
-      if (i%2):
-        newconfig[k] =  mate.config[k]
-      else:
-        newconfig[k] = self.config[k]
-      i = i+1
+        # and save it
+        self.config = newconfig.copy()
 
-    """ and save it """
-    self.config = newconfig.copy()
+    def mutate_random(self, percent, number_of_mutations):
+        randomkeys = self.config.keys()
+        random.shuffle(randomkeys)
+        randomkeys = randomkeys[0:number_of_mutations]
 
-  def mutate_random(self, percent, number_of_mutations):
-    randomkeys = self.config.keys()
-    random.shuffle(randomkeys)
-    randomkeys = randomkeys[0:number_of_mutations]
+        for key in randomkeys:
+            self.mutate_one_config(key, percent)
 
-    for key in randomkeys:
-     self.mutate_one_config(key, percent)
+    def mutate_one_config(self, configkey, percent):
+        value = self.config[configkey]
+        key = self._options[configkey]
+        variant = 0
+        if key == 'memory':
+            """ integer between 1kb and 1024MB """
+            variant = random.randint(1, 1024 * 1024 * 1024)
+            variant = variant * (percent / 100.0)
+            variant = variant - (1024 * 1024 * 1024 * percent / 100.0) / 2.0
+            variant = int(variant)
+        elif key == '0.0to1.0':
+            variant = round(random.random(), 3)
+            variant = variant * (percent / 100.0)
+            variant = variant - (0.5 * (percent / 100.0))
+        elif key == 'uint':
+            variant = random.randint(1, 1024)
+            variant = variant * (percent / 100.0)
+            variant = variant - (1024.0 * percent / 100.0) / 2.0
+            variant = int(variant)
+        else:
+            """ nuffin """
 
-  def mutate_one_config(self, configkey, percent):
-    value = self.config[configkey]
-    key = self._options[configkey]
-    variant = 0
-    if key == 'memory':
-      """ integer between 1kb and 1024MB """
-      variant = random.randint(1, 1024*1024*1024)
-      variant = variant * (percent/100.0)
-      variant = variant - (1024*1024*1024*percent/100.0)/2.0
-      variant = int(variant)
-    elif key == '0.0to1.0':
-      variant = round(random.random(), 3)
-      variant = variant * (percent/100.0)
-      variant = variant - (0.5*(percent/100.0))
-    elif key == 'uint':
-      variant = random.randint(1, 1024)
-      variant = variant * (percent/100.0)
-      variant = variant - (1024.0 * percent/100.0)/2.0
-      variant = int(variant)
-    else:
-      """ nuffin """
+        value = value + variant
+        if (value < 0):
+            value = 0
 
-    value = value+variant
-    if (value < 0):
-      value = 0
+        self.config[configkey] = value
 
-    self.config[configkey] = value
+    def _memory_value_from_int(self, val):
+        if (val <= 2 * 1024 * 1024):
+            return '%dkB' % val
+        else:
+            return '%dMB' % (val / (1024 * 1024))
 
+    def print_out_to_file(self, file_name):
+        f = open(file_name, 'w')
+        print >> f, self.get_config()
+        f.close()
 
+    def print_out(self):
+        print self.get_config()
 
-  def _memory_value_from_int(self, val):
-    if (val <= 2*1024*1024):
-      return '%dkB' % val
-    else:
-      return '%dMB' % (val/(1024*1024))
-
-
-
-  def print_out_to_file(self, file_name):
-    f = open(file_name, 'w')
-    print >> f, self.get_config()
-    f.close()
-
-
-
-  def print_out(self):
-    print self.get_config()
-
-
-
-  def get_config(self):
-    """ put the config into the return string """
-    config=""
-    for option in self.config:
-      stroption = str(self.config[option])
-      if self._options[option] == 'memory':
-        config = config + '\n' + option + '\t= ' + self._memory_value_from_int(self.config[option])
-      else:
-        config = config + '\n' + option + '\t= ' + stroption
-    return config
-
-
-
-
+    def get_config(self):
+        """
+        put the config into the return string
+        """
+        config = ""
+        for option in self.config:
+            stroption = str(self.config[option])
+            if self._options[option] == 'memory':
+                memory_val = self._memory_value_from_int(self.config[option])
+                config = config + '\n' + option + '\t= ' + memory_val
+            else:
+                config = config + '\n' + option + '\t= ' + stroption
+        return config
